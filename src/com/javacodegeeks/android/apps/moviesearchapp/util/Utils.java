dc916d40bb132ce6dd181b4f475ba5d8add58f5f
unknown <nicks@nicks-PC.(none)>
2012-04-07 14:41:05 +0530

    First commit full source of movie search app works via xml parsing of rss feeds.

---
+++ src/com/javacodegeeks/android/apps/moviesearchapp/util/Utils.java
@@ -0,0 +1,18 @@
+package com.javacodegeeks.android.apps.moviesearchapp.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Utils {
+	
+	public static void closeStreamQuietly(InputStream inputStream) {
+		 try {
+			if (inputStream != null) {
+			     inputStream.close();  
+			 }
+		} catch (IOException e) {
+			// ignore exception
+		}
+	}
+
+}
